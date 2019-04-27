a4c55fdc4cd3bf1f1e01c1116a4057558fe04a31
shivapbhusal <civabhusal@gmail.com>
2019-04-27 18:14:58 -0400

    finalize analyze_errors

--- analysis/analyze_errors.py
+++ analysis/analyze_errors.py
@@ -2,11 +2,28 @@
 This program analyzes common errors in Python repos analysed.
 '''
 import re
+import sys
 
-pattern = re.compile(r'C[0-9]|E[0-9]|F[0-9]|I[0-9]|R[0-9]|W[0-9]|RP[0-9]')
-errors = re.findall(pattern, 'TESTC00RP000TEST')
 
-print(errors)
+error_dict = dict()
+
+result_file = sys.argv[1]
+pattern = re.compile(r'C[0-9]{4}|E[0-9]{4}|F[0-9]{4}|I[0-9]{4}|R[0-9]{4}|W[0-9]{4}|RP[0-9]{4}')
+
+with open(result_file, 'r') as f:
+    for line in f:
+        errors = re.findall(pattern, line)
+        if errors:
+            for error in errors:
+                error = error.strip()
+                if error in error_dict:
+                    error_dict[error]+=1
+                else:
+                    error_dict[error]=1
+    f.close()
+
+for error_type in error_dict:
+    print(error_type +':' + str(error_dict[error_type]))
 
 
 
